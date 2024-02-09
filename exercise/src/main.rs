28d43359ae096c8a3e3669298a8e82506663c30d
Richad Ye <richardyeee@outlook.com>
2024-02-09 15:13:44 -0500

    exercises up to day 1 section 9

---
+++ exercise/src/main.rs
@@ -0,0 +1,64 @@
+// Exercise 6.7 - Control Flow: Collatz sequence
+fn collatz_length(mut n: i32) -> u32 {
+    let mut count = 1;
+    while n > 1 {
+        n = if n % 2 == 0 {
+            n/2
+        } else {
+            3 * n + 1
+        };
+        count+=1;
+    }
+    count
+}
+
+// Exercise 8.5 - Tuples and Arrays: Nested Arrays
+fn transpose(matrix: [[i32; 3]; 3]) -> [[i32; 3]; 3] {
+    let mut transposed: [[i32; 3]; 3] = matrix; 
+    for row in 0..3{
+        for col in 0..3{
+            transposed[col][row] = matrix[row][col];
+        }
+    }
+    transposed
+} 
+
+// Exercise 9.3 - References: Geometry
+
+// Calculate the magnitude of a vector by summing the squares of its coordinates
+// and taking the square root. Use the `sqrt()` method to calculate the square
+// root, like `v.sqrt()`.
+fn magnitude(vector: &([f64; 3], [f64; 3])) -> f64 {
+    let xMag = *vector[1][0] - *vector[0][0];
+    let yMag = *vector[1][1] - *vector[0][1];
+    let zMag = *vector[1][2] - *vector[0][2];
+    
+    let mut mag = xMag.pow(2) + yMag.pow(2) + zMag.pow(2);
+    mag.sqrt();
+}
+
+// Normalize a vector by calculating its magnitude and dividing all of its
+// coordinates by that magnitude.
+fn normalize(vector: &([f64; 3], [f64; 3])) -> ([f64; 3], [f64; 3]) {
+    
+}
+
+fn main() {
+    println!("Exercise 6.7 - Control Flow: {}", collatz_length(256));
+
+
+    let matrix = [
+        [101, 102, 103], // <-- the comment makes rustfmt add a newline
+        [201, 202, 203],
+        [301, 302, 303],
+    ];  
+    println!("Exercise 8.5 - Nested Arrays: original {:?} \n transposed {:#?}", matrix, transpose(matrix));
+
+
+    println!("Magnitude of a unit vector: {}", magnitude(&[0.0, 1.0, 0.0]));
+
+    let mut v = [1.0, 2.0, 9.0];
+    println!("Magnitude of {v:?}: {}", magnitude(&v));
+    normalize(&mut v);
+    println!("Magnitude of {v:?} after normalization: {}", magnitude(&v));
+}
